4f9c8659d83f5a76c129089166f29cf0e4c39d1a
SebastianUVG <gar22291@uvg.edu.gt>
2025-03-27 17:21:59 -0600

    New files

--- main.go
+++ main.go
@@ -21,8 +21,26 @@ func getMatch(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, matches)
 }
 
+func createMatch(c *gin.Context) {
+	var newMatch Match
+
+	if err := c.BindJSON(&newMatch); err != nil {
+		return
+	}
+
+	matches = append(matches, newMatch)
+	c.IndentedJSON(http.StatusCreated, newMatch)
+
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/matches", getMatch)
+
+	api := router.Group("/api")
+	{
+		api.GET("/matches", getMatch)
+		api.POST("/matches", createMatch)
+	}
+
 	router.Run("localhost:8080")
 }
